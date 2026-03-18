fa9be52798e2b4d89975eff4469124a09fa6f518
getinn2026-boop <getinn.2026@gmail.com>
2026-03-18 11:43:32 +0530

    Fix PrintButton import

--- src/app/quotes/[id]/print/page.tsx
+++ src/app/quotes/[id]/print/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { prisma } from "@/server/db";
-import PrintButton from "./PrintButton";
+import { PrintButton } from "./PrintButton";
 
 export const dynamic = 'force-dynamic';
 
